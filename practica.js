9f78fdcbcf68e420cff288306ade5a9236013728
Dario Manuel <DMANUEL@ferrosur.com.ar>
2023-12-29 19:16:41 -0300

    5to commit

--- practica.js
+++ practica.js
@@ -879,3 +879,88 @@ palindromos("agua");
         :console.info(cadena.replace(new RegExp(patron, "gi"), ""));
 
 eliminarPatron("xyz1, xyz2, xyz3, xyz4 y xyz5", "xyz");
+
+/*
+    function findFirstRepeated(gifts) {
+        const mapa = {}
+
+        for(const numero of gifts) {
+            if(mapa[numero]) return numero
+            mapa[numero] = true
+        }
+        return -1;
+    }
+*/
+//9) Programa una función que obtenga un numero aleatorio entre 501 y 600.
+
+const aleat = () =>
+{
+    min = 501;
+    max = 600;
+    return Math.round(Math.random()* (max - min) + min);
+}
+aleat();
+
+//10) Programa una función que reciba un número y evalúe si es capicúa o no (que se lee igual en un sentido que en otro), pe. miFuncion(2002) devolverá true.
+
+const capicuas = (numero = 0) => {
+
+    if(!numero) return console.warn("No ingresaste número");
+    if(typeof numero !== "number" || isNaN(numero)) return console.warn("Solo se aceptan caracteres numericos");
+    
+    numero = numero.toString();
+    let numeroAlrevez = numero.split("").reverse().join("");
+
+    return (numeroAlrevez === numero)
+}
+
+// capicuas(17071);
+// capicuas(2002);
+// capicuas("asd");
+// capicuas(235);
+
+//11) Programa una función que calcule el factorial de un número (El factorial de un entero positivo n, se define como el producto de todos los números enteros positivos desde 1 hasta n), pe. miFuncion(5) devolverá 120.
+
+const factoriales = (numero = 0) => {
+if(!numero) return console.warn("No ingresaste número");
+    if(typeof numero !== "number" || isNaN(numero)) return console.warn("Solo se aceptan caracteres numericos");
+    if(numero < 0) return console.error("No existe factorial de números negativos");
+    if(numero === 0 || numero === 1) return 1;
+
+    contador = 1;
+
+    for(let i = 1; i <= numero; i++){
+        contador *= i;
+    }
+    return contador;
+
+}
+
+// factoriales();
+// factoriales("5");
+// factoriales(-1);
+// factoriales(5);
+
+//12) Programa una función que determine si un número es primo (aquel que solo es divisible por sí mismo y 1) o no, pe. miFuncion(7) devolverá true.
+
+const esPrimo = (numero = 0) => {
+    if(!numero) return console.warn("No ingresaste número");
+    if(typeof numero !== "number" || isNaN(numero)) return console.warn("Solo se aceptan caracteres numericos");
+    if(numero === 0 || numero === 1 || numero === 4) return false;
+
+    let divisible = false;
+    for(let i = 2; i < numero; i++){
+    
+        if(numero % i === 0){
+        divisible = true;
+    break;
+}
+}
+return !divisible;
+}
+esPrimo(7);
+esPrimo(8);
+
+//13) Programa una función que determine si un número es par o impar, pe. miFuncion(29) devolverá Impar.
+
+//14) Programa una función para convertir grados Celsius a Fahrenheit y viceversa, pe. miFuncion(0,"C") devolverá 32°F.
