df28b7efb6405ecefa880b472609d90f6e47aa8c
Dario Manuel <DMANUEL@ferrosur.com.ar>
2024-01-13 17:57:15 -0300

    mas ejercicios

--- practica.js
+++ practica.js
@@ -1495,3 +1495,81 @@ const peli = new Pelicula(
 );
 
 peli.fichaTecnica();
+
+// Escriba un ciclo que haga siete llamadas a console.log para generar el siguiente
+// triángulo:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for(let i = 0; i <= 7; i++ ){
+    let linea = "";
+        for (let j = 0; j < i; j++) {
+            linea += "#";
+    }
+    console.log(linea);
+}
+
+// FizzBuzz
+// Escribe un programa que use console.log para imprimir todos los números de
+// 1 a 100, con dos excepciones. Para números divisibles por 3, imprime "Fizz"
+// en lugar del número, y para los números divisibles por 5 (y no 3), imprime
+// "Buzz" en su lugar.
+// Cuando tengas eso funcionando, modifica tu programa para imprimir "
+// FizzBuzz", para números que sean divisibles entre 3 y 5 (y aún imprimir "Fizz"
+// o "Buzz" para números divisibles por solo uno de ellos).
+
+const FizzBuzz = () => {
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("FizzBuzz");
+        }
+        else if (i % 3 === 0) {
+            console.log("Fizz");
+        }
+        else if (i % 5 === 0) {
+            console.log("Buzz");
+        }
+        else {
+            console.log(i);
+        }
+    }
+}
+
+// Tablero de ajedrez
+// Escribe un programa que cree un string que represente una cuadrícula de 8 ×
+// 8, usando caracteres de nueva línea para separar las líneas. En cada posición
+// de la cuadrícula hay un espacio o un carácter "#". Los caracteres deberían de
+// formar un tablero de ajedrez.
+// Pasar este string a console.log debería mostrar algo como esto:
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+//  # # # #
+// # # # #
+const boardSize = 8;
+const chessBoard = () => {
+    let board = "";
+    for (let i = 0; i < boardSize; i++) {
+        for (let j = 0; j < boardSize; j++) {
+            if ((i + j) % 2 === 0) {
+                board += " ";
+            }
+            else {
+                board += "#";
+            }
+        }
+        board += "\n";
+    }
+    return board;
+}
+// Cuando tengas un programa que genere este patrón, define una vinculación
+// tamaño = 8 y cambia el programa para que funcione con cualquier tamaño,
+// dando como salida una cuadrícula con el alto y ancho dados.
