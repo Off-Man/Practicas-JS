7f9f376386865b559f3bbb6e6ef9a381c77e1c0d
Dario Manuel <dariomanuel999@gmail.com>
2024-02-11 19:00:15 -0300

    nuevos

--- practica.js
+++ practica.js
@@ -1999,3 +1999,11 @@ function direccionDominante(texto) {
 
 console.log(direccionDominante("Hello!")); // → ltr
 
+console.log(direccionDominante("Hello!")); // → ltr
+
+function normalizar() {
+    console.log(this.coordinadas.map(n => n / this.length));
+    }
+    normalizar.call({coordinadas: [0, 2, 3], length: 5});
+    // → [0, 0.4, 0.6]
+
